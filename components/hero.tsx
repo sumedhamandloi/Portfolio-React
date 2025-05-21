84eadd43fbc9a2d07f795a7c251db35df5c1b76a
sumedhamandloi <sumedhamandloi@gmail.com>
2025-05-21 21:59:18 +0530

    Update hero.tsx

--- components/hero.tsx
+++ components/hero.tsx
@@ -3,10 +3,17 @@ export default function Hero() {
     <section id="about" className="py-20 px-4 sm:px-6 lg:px-8 max-w-6xl mx-auto">
       <div className="text-center">
         <h1 className="text-5xl md:text-6xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-purple-600 to-teal-500 mb-6">
-          Hi, I&apos;m Rachit neema
+          Hi, I&apos;m Sumedha Mandloi
         </h1>
         <p className="text-lg md:text-xl text-gray-600 max-w-3xl mx-auto">
-          Welcome to my portfolio! I'm a second-year IT engineering student with a passion for learning and building with technology. This space is where I showcase my skills, projects, and growth as I explore the ever-evolving world of tech. I'm currently diving into new tools, languages, and frameworks—and always on the lookout for exciting opportunities to learn, collaborate, and gain real-world experience..
+          I am an IT undergrad with a keen interest in AI advancements. My goal is to leverage my skills in Python, HTML, CSS, JavaScript, and MySQL to contribute to the ever-evolving field of technology and AI. 
+
+Proficient with - HTML, CSS, Javascript 
+Basic knowledge of - Python, SQL
+Learning - React
+Technologies I wish to explore - LLMs, NLP, Machine Learning, DSA
+
+Apart from these, I am enthusiastic about designing, learning different languages and introducing myself to new cultures. I am eager to connect with professionals and enthusiasts in the tech industry to learn, grow, and explore new opportunities.
         </p>
       </div>
     </section>
